2681b51d6e2476930eca66b84d2b1fbd7111696a
hoang <hoangtu2378@gmail.com>
2021-05-18 21:10:38 +0700

    lab 1

---
+++ T2008M_AP/lab1/Product.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+
+namespace T2008M_AP.lab1
+{
+    public class Product
+    {
+        public int id;
+        public string name;
+        public int qty;
+        public string image;
+        public string desc;
+        public ArrayList gallery = new ArrayList();
+
+        public Product()
+        {
+        }
+
+        public Product(int id, string name, int qty, string image, string desc, ArrayList gallery)
+        {
+            this.id = id;
+            this.name = name;
+            this.qty = qty;
+            this.image = image;
+            this.desc = desc;
+            this.gallery = gallery;
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("id: " + id);
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("qty: " + qty);
+            Console.WriteLine("image: " + image);
+            Console.WriteLine("desc: " + desc);
+            Console.WriteLine("gallery: ");
+            foreach (var item in gallery)
+            {
+                Console.Write(item + "\n");
+            }
+        }
+
+        public bool checkQty()
+        {
+            if (qty > 0 )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void addImage()
+        {
+            int c = 8;
+            for (int i = 0; i < gallery.Count; i++)
+            {
+                if (gallery.Count < 10)
+                {
+                    Console.Write("Them anh: ");
+                    string img = Console.ReadLine();
+                    gallery.Add(c + " - " + img);
+                    c++;
+                }
+                else
+                {
+                    Console.Write("So luong anh da vuot qua 10, moi ban xoa bo 1 anh! \n");
+                    return;
+                }
+            }
+            
+            
+        }
+
+        public void deleteImage()
+        {
+            foreach (var item in gallery)
+            {
+                Console.Write(item + "\n");
+            }
+            
+            Console.WriteLine("Vui long chon anh muon xoa: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < gallery.Count; i++)
+            {
+                if (n == i)
+                {
+                    gallery.Remove(i);
+                    Console.Write("Da xoa " + gallery[i] + "\n");
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ T2008M_AP/lab1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace T2008M_AP.lab1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Product pr = new Product();
+            pr.id = 1;
+            pr.name = "Iphone 13";
+            pr.qty = 10;
+            pr.image = "Anh dep";
+            pr.desc = "xuong";
+            pr.gallery.Add("0 - anh 1");
+            pr.gallery.Add("1 - anh 2");
+            pr.gallery.Add("2 - anh 3");
+            pr.gallery.Add("3 - anh 4");
+            pr.gallery.Add("4 - anh 5");
+            pr.gallery.Add("5 - anh 6");
+            pr.gallery.Add("6 - anh 7");
+            pr.gallery.Add("7 - anh 8");
+            pr.getInfo();
+            if (pr.checkQty())
+            {
+                Console.WriteLine("con hang");
+            }
+            else
+            {
+                Console.WriteLine("het hang");
+            }
+            pr.addImage();
+            pr.deleteImage();
+        }
+    }
+}

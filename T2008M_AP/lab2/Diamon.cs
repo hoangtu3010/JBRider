3ac14e7e2967b901da1b9170da930066e89bbd74
hoang <hoangtu2378@gmail.com>
2021-05-19 10:08:56 +0700

    lab 2

---
+++ T2008M_AP/lab2/Diamon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using T2008M_AP.lab1;
+
+namespace T2008M_AP.lab2
+{
+    public class Diamon : Product
+    {
+        public int cara;
+
+        public bool checkAuFa()
+        {
+            if (cara > 1)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ T2008M_AP/lab2/Fashion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using T2008M_AP.lab1;
+
+namespace T2008M_AP.lab2
+{
+    public class Fashion : Product
+    {
+        public string color;
+        public string size;
+
+        public bool checkColorSize(string c, string s)
+        {
+            if (color.Equals(c) && size.Equals(s) && qty > 0)
+                return true;
+            return false;
+        }
+    }
+}
